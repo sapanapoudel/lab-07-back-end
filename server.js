131231dfaff89747ca08cf307363585a18ca35bd
FabianB14 <32110808+FabianB14@users.noreply.github.com>
2019-07-10 11:10:42 -0700

    new adds

--- server.js
+++ server.js
@@ -34,6 +34,16 @@ app.get('/weather', (request, response) => {
     response.status(500).send('Status 500, not functional.');
   }
 });
+app.get('/',(request,response) =>{
+  try{
+    response.send('server live');
+  }catch(e){
+    response.status(500).send('Status 500, not functional.');
+  }
+});
+app.use('*',(request,response)=>{
+  response.send('you got to the wrong place')
+})
 
 function Location(name, formatted, lat, lng) {
   this.search_query = name;
